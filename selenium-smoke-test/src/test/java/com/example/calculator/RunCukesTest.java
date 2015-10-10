6f06a5dbe17ea27360a4556e11043231bc7ef836
Jacob D. Parr <me@jacobparr.com>
2015-10-09 21:10:26 -0700

    Added selenium-smoke-test

---
+++ selenium-smoke-test/src/test/java/com/example/calculator/RunCukesTest.java
@@ -0,0 +1,17 @@
+package com.example.calculator;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Runnable driver class for running Cucumber feature tests.
+ * 
+ * @author @techjedi
+ *
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = { "json:target/cucumber-report.json", "html:target/cucumber-report.html" })
+public class RunCukesTest {
+
+}
---
+++ selenium-smoke-test/src/test/java/com/example/shopping/GlobalConfiguration.java
@@ -0,0 +1,15 @@
+package com.example.shopping;
+
+/**
+ * Global configuration for parameterizing SHOP_ADDRESS in tests.
+ * 
+ * @author @nevenc
+ *
+ */
+public class GlobalConfiguration {
+
+    // if the shop address is not valid, please create a new sample shop at
+    // http://spreecommerce.com, replace SHOP_ADDRESS with sample shop URL
+    public final static String SHOP_ADDRESS = "https://majestic-emporium-9828.spree.mx/";
+
+}
