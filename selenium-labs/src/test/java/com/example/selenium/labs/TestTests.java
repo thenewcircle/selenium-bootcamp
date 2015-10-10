717f58c4f6f4ae7c7109b066c68c64e4544565a9
Jacob D. Parr <me@jacobparr.com>
2015-10-10 12:33:31 -0700

    Added simple unit test

---
+++ selenium-labs/src/test/java/com/example/selenium/labs/TestTests.java
@@ -0,0 +1,13 @@
+package com.example.selenium.labs;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestTests {
+
+	@Test
+	public void testSomething() {
+		Assert.assertTrue(true);
+	}
+	
+}
