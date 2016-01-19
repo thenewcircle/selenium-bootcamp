ce24410af3f3b20c52d01a0516643d15c2929220
Jacob D. Parr <me@jacobparr.com>
2016-01-19 13:05:11 -0500

    Web Drivers

--- src/test/java/com/example/selenium/spree/ShoppingSpreeTests.java
+++ src/test/java/com/example/selenium/spree/ShoppingSpreeTests.java
@@ -1,27 +1,43 @@
 package com.example.selenium.spree;
 
-import ch.qos.logback.classic.Level;
-import com.example.selenium.LogbackUtils;
-import com.gargoylesoftware.htmlunit.DefaultCssErrorHandler;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import com.example.selenium.LogbackUtils;
+import com.gargoylesoftware.htmlunit.DefaultCssErrorHandler;
+import com.gargoylesoftware.htmlunit.javascript.StrictErrorReporter;
+
+import ch.qos.logback.classic.Level;
 
 public class ShoppingSpreeTests {
 
   static {
     LogbackUtils.initLogback(Level.WARN);
     LogbackUtils.setLevel(DefaultCssErrorHandler.class, Level.ERROR);
+    LogbackUtils.setLevel(StrictErrorReporter.class, Level.OFF);
   }
 
   private WebDriver browser;
 
   @Before
   public void beforeMethod() throws Exception {
-    browser = new HtmlUnitDriver();
+//    HtmlUnitDriver webDriver = new HtmlUnitDriver();
+//    webDriver.setJavascriptEnabled(true);
+//    FirefoxDriver webDriver = new FirefoxDriver();
+
+//    String path = System.getenv("webdriver.chrome.driver");
+//    System.setProperty("webdriver.chrome.driver", path);
+//    ChromeDriver webDriver = new ChromeDriver();
+    
+    String path = System.getenv("webdriver.ie.driver");
+    System.setProperty("webdriver.ie.driver", path);
+    InternetExplorerDriver webDriver = new InternetExplorerDriver();
+
+    browser = webDriver;
   }
 
   @Test
