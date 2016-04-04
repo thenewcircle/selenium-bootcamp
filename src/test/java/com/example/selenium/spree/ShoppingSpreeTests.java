a59acff0bca715ff3c695edea48fd0518663ce57
Jacob D. Parr <me@jacobparr.com>
2016-04-04 15:05:01 -0700

    Working with parameterized tests.

--- src/test/java/com/example/selenium/spree/ShoppingSpreeTests.java
+++ src/test/java/com/example/selenium/spree/ShoppingSpreeTests.java
@@ -1,27 +1,39 @@
 package com.example.selenium.spree;
 
-import ch.qos.logback.classic.Level;
-import com.example.selenium.LogbackUtils;
-import com.gargoylesoftware.htmlunit.DefaultCssErrorHandler;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import com.example.selenium.LogbackUtils;
+//import com.gargoylesoftware.htmlunit.DefaultCssErrorHandler;
+
+import ch.qos.logback.classic.Level;
+
+@RunWith(Parameterized.class)
 public class ShoppingSpreeTests {
 
   static {
     LogbackUtils.initLogback(Level.WARN);
-    LogbackUtils.setLevel(DefaultCssErrorHandler.class, Level.ERROR);
+//    LogbackUtils.setLevel(DefaultCssErrorHandler.class, Level.ERROR);
   }
 
-  private WebDriver webDriver;
+  private RemoteWebDriver webDriver;
+  private final WebDriverFactory wdf;
+  
+  public ShoppingSpreeTests(WebDriverFactory wdf, String name) {
+    this.wdf = wdf;
+  }
 
   @Before
   public void beforeMethod() throws Exception {
-    webDriver = new HtmlUnitDriver();
+    webDriver = wdf.create();
   }
 
   @Test
@@ -33,5 +45,15 @@ public class ShoppingSpreeTests {
   @After
   public void afterMethod() {
     webDriver.quit();
+  }
+  
+  @Parameterized.Parameters(name = "{1}")
+  public static Iterable<Object[]> createTestParameters() {
+    List<Object[]> data = new ArrayList();
+    data.add(new Object[]{ new FirefoxDriverFactory(),          "Firefox" });
+    data.add(new Object[]{ new ChromeDriverFactory(),           "Chrome" });
+    // data.add(new Object[]{ new SafariDriverFactory(),           "Safari" });
+    // data.add(new Object[]{ new InternetExplorerDriverFactory(), "IE" });
+    return data;
   }
 }
