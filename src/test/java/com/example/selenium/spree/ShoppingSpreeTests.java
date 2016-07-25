45e7c0744bfa52d70534848ad0014493f31b4ab2
Jacob D Parr <me@jacobparr.com>
2016-07-25 14:44:17 -0700

    Lab 9.2

--- src/test/java/com/example/selenium/spree/ShoppingSpreeTests.java
+++ src/test/java/com/example/selenium/spree/ShoppingSpreeTests.java
@@ -1,9 +1,19 @@
 package com.example.selenium.spree;
 
-import org.testng.Assert;
-import org.testng.annotations.*;
 import ch.qos.logback.classic.Level;
 import com.example.selenium.spree.support.LogbackUtils;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Factory;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShoppingSpreeTests {
 
@@ -11,23 +21,57 @@ public class ShoppingSpreeTests {
         LogbackUtils.initLogback(Level.WARN);
     }
 
-    @BeforeMethod
+    enum DriverType { Chrome, Firefox, IE, Safari, Edge }
+    DriverType driverType;
+
+    RemoteWebDriver webDriver;
+
+    public ShoppingSpreeTests(DriverType driverType) {
+        this.driverType = driverType;
+    }
+
+    @Factory()
+    public static Object[] testFactory() {
+        List<ShoppingSpreeTests> data = new ArrayList<>();
+        data.add(new ShoppingSpreeTests(DriverType.Chrome));
+        data.add(new ShoppingSpreeTests(DriverType.IE));
+        // data.add(new ShoppingSpreeTests(DriverType.Safari));
+        data.add(new ShoppingSpreeTests(DriverType.Firefox));
+        return data.toArray();
+    }
+
     public void beforeMethod() throws Exception {
-        // save for later
+        if (DriverType.Safari == driverType) {
+            webDriver = new SafariDriver();
+
+        } else if (DriverType.Firefox == driverType) {
+            webDriver = new FirefoxDriver();
+
+        } else if (DriverType.Chrome == driverType) {
+            String path = System.getenv("webdriver.chrome.driver");
+            System.setProperty("webdriver.chrome.driver", path);
+            webDriver = new ChromeDriver();
+
+        } else if (DriverType.IE == driverType) {
+            String path = System.getenv("webdriver.ie.driver");
+            System.setProperty("webdriver.ie.driver", path);
+            webDriver = new InternetExplorerDriver();
+
+        } else {
+            throw new UnsupportedOperationException(driverType.name());
+        }
     }
 
-    @Test
-    public void testA() {
-        Assert.assertTrue(true);
-    }
 
     @Test
-    public void testB() {
-        Assert.assertEquals(1, 1);
+    public void testHomePage() {
+        webDriver.get("http://spree.newcircle.com");
+        String title = webDriver.getTitle();
+        Assert.assertEquals(title, "Spree Demo Site");
     }
 
     @AfterMethod
     public void afterMethod() {
-        // save for later
+        webDriver.quit();
     }
 }
