be92d2a3c33a7d49b6dae52147173ae24ca54731
Jacob D. Parr <me@jacobparr.com>
2016-11-02 14:40:23 -0700

    Parameterized tests working

--- java-course/src/test/java/com/example/selenium/spree/ShoppingSpreeTests.java
+++ java-course/src/test/java/com/example/selenium/spree/ShoppingSpreeTests.java
@@ -1,45 +1,91 @@
 package com.example.selenium.spree;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 import org.testng.Assert;
+import org.testng.ITest;
 import org.testng.annotations.*;
 import ch.qos.logback.classic.Level;
 import com.example.selenium.spree.support.LogbackUtils;
 
-public class ShoppingSpreeTests {
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingSpreeTests implements ITest {
+
+    enum DriverType { Chrome, Firefox, IE, Safari, Edge }
+    DriverType driverType;
+    String testName;
 
     static {
         LogbackUtils.initLogback(Level.WARN);
     }
 
-    @BeforeClass
-    public void beforeClass() throws Exception {
-        System.out.println("Before class");
+    RemoteWebDriver webDriver;
+
+    public ShoppingSpreeTests(DriverType driverType) {
+        this.driverType = driverType;
+    }
+
+    @Override
+    public String getTestName() {
+        return testName;
     }
 
     @BeforeMethod
-    public void beforeMethod() throws Exception {
-        System.out.println("Before method");
+    public void beforeMethod(Method aMethod) throws Exception {
+        testName = aMethod.getName() + " [" + driverType + "]";
+
+        if (DriverType.Safari == driverType) {
+            webDriver = new SafariDriver();
+
+        } else if (DriverType.Firefox == driverType) {
+            String path = System.getenv("webdriver_firefox_marionette");
+            System.setProperty("webdriver.firefox.marionette", path);
+            webDriver = new FirefoxDriver();
+
+        } else if (DriverType.Chrome == driverType) {
+            String path = System.getenv("webdriver_chrome_driver");
+            System.setProperty("webdriver.chrome.driver", path);
+            webDriver = new ChromeDriver();
+
+        } else if (DriverType.IE == driverType) {
+            String path = System.getenv("webdriver_ie_driver");
+            System.setProperty("webdriver.ie.driver", path);
+            webDriver = new InternetExplorerDriver();
+
+        } else {
+            throw new UnsupportedOperationException(driverType.name());
+        }
     }
 
     @Test
-    public void testA() {
-        System.out.println("Test A");
-        Assert.assertEquals(1, 2);
+    public void testA() throws Exception {
+        Assert.assertTrue(true);
     }
 
     @Test
     public void testB() {
-        System.out.println("Test B");
         Assert.assertEquals(1, 1);
     }
 
     @AfterMethod
     public void afterMethod() {
-        System.out.println("After method\n");
+        webDriver.quit();
     }
 
-    @AfterClass
-    public void afterClass() {
-        System.out.println("After class");
+    @Factory()
+    public static Object[] testFactory() {
+        List<ShoppingSpreeTests> data = new ArrayList<>();
+        data.add(new ShoppingSpreeTests(DriverType.IE));
+        // data.add(new ShoppingSpreeTests(DriverType.Safari));
+        data.add(new ShoppingSpreeTests(DriverType.Firefox));
+        data.add(new ShoppingSpreeTests(DriverType.Chrome));
+        return data.toArray();
     }
 }
