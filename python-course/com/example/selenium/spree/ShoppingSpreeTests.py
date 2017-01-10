10684400a13e8bd1d5ac74036d043761d65c20d8
Jacob D. Parr <me@jacobparr.com>
2017-01-10 13:37:55 -0800

    Enum for driver type to switch between browsers.

--- python-course/com/example/selenium/spree/ShoppingSpreeTests.py
+++ python-course/com/example/selenium/spree/ShoppingSpreeTests.py
@@ -1,29 +1,44 @@
 import os
 import unittest
+from enum import Enum
 
 from selenium import webdriver
+
+class DriverType(Enum):
+    Chrome = 1
+    Firefox = 2
+    IE = 3
+    Safari = 4
+    Edge = 5
 
 class ShoppingSpreeTests(unittest.TestCase):
 
     def setUp(self):
-        # self.webDriver = webdriver.Firefox(executable_path="c:\\tools\\selenium\\geckodriver.exe")
-
-        path = os.environ["webdriver_chrome_driver"]
-        self.webDriver = webdriver.Chrome(executable_path=path)
-
-        # self.webDriver = webdriver.Ie(executable_path="c:\\tools\\selenium\\IEDriverServer.exe")
-        # self.webDriver = webdriver.Safari()
-
+        driver_type = DriverType.Firefox
+    
+        if DriverType.Firefox == driver_type:
+            path = os.environ["webdriver_firefox_marionette"]
+            self.webDriver = webdriver.Firefox(executable_path=path)
+        
+        elif DriverType.Chrome == driver_type:
+            path = os.environ["webdriver_chrome_driver"]
+            self.webDriver = webdriver.Chrome(executable_path=path)
+        
+        elif DriverType.IE == driver_type:
+            path = os.environ["webdriver_ie_driver"]
+            self.webDriver = webdriver.Ie(executable_path=path)
+        
+        elif DriverType.Edge == driver_type:
+            path = os.environ["webdriver_edge_driver"]
+            self.webDriver = webdriver.Edge(executable_path=path)
+        
+        elif DriverType.Safari == driver_type:
+            self.webDriver = webdriver.Safari()
+            
     def testHomePage(self):
         self.webDriver.get("https://spreecommerce-demo.herokuapp.com")
         title = self.webDriver.title
         self.assertEquals("Spree Demo Site", title)
     
-    def testA(self):
-        self.assertTrue(True)
-
-    def testB(self):
-        self.assertEquals(1, 1)
-
     def tearDown(self):
         self.webDriver.quit()
