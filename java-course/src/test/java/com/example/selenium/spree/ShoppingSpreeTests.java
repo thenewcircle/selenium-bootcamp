2e9a256874948ddd8bd04a0d96e618c22e12c89b
Jacob D. Parr <me@jacobparr.com>
2017-01-11 00:12:26 -0800

    Completed Lab 9.c (except for Scala)

--- java-course/src/test/java/com/example/selenium/spree/ShoppingSpreeTests.java
+++ java-course/src/test/java/com/example/selenium/spree/ShoppingSpreeTests.java
@@ -10,28 +10,38 @@ import org.testng.annotations.*;
 import ch.qos.logback.classic.Level;
 import com.example.selenium.spree.support.LogbackUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingSpreeTests {
 
     static {
         LogbackUtils.initLogback(Level.WARN);
     }
 
+    enum DriverType { Chrome, Firefox, IE, Safari, Edge }
+
+    DriverType driverType;
     RemoteWebDriver webDriver;
+
+    public ShoppingSpreeTests(DriverType driverType) {
+        this.driverType = driverType;
+    }
 
     @BeforeMethod
     public void beforeMethod() throws Exception {
-//        System.setProperty("webdriver.gecko.driver", "c:\\tools\\selenium\\geckodriver.exe");
-//        webDriver = new FirefoxDriver();
+        // System.setProperty("webdriver.gecko.driver", "c:\\tools\\selenium\\geckodriver.exe");
+        // webDriver = new FirefoxDriver();
 
-//        String path = System.getenv("webdriver_chrome_driver");
-//        System.setProperty("webdriver.chrome.driver", path);
-//        webDriver = new ChromeDriver();
+        String path = System.getenv("webdriver_chrome_driver");
+        System.setProperty("webdriver.chrome.driver", path);
+        webDriver = new ChromeDriver();
 
-//        String path = System.getenv("webdriver_ie_driver");
-//        System.setProperty("webdriver.ie.driver", path);
-//        webDriver = new InternetExplorerDriver();
+        // String path = System.getenv("webdriver_ie_driver");
+        // System.setProperty("webdriver.ie.driver", path);
+        // webDriver = new InternetExplorerDriver();
 
-        webDriver = new SafariDriver();
+        // webDriver = new SafariDriver();
     }
 
     @Test
@@ -56,4 +66,13 @@ public class ShoppingSpreeTests {
         webDriver.quit();
     }
 
+    @Factory()
+    public static Object[] testFactory() {
+        List<ShoppingSpreeTests> data = new ArrayList<>();
+        data.add(new ShoppingSpreeTests(DriverType.Chrome));
+        data.add(new ShoppingSpreeTests(DriverType.Firefox));
+        // data.add(new ShoppingSpreeTests(DriverType.IE));
+        // data.add(new ShoppingSpreeTests(DriverType.Safari));
+        return data.toArray();
+    }
 }
--- python-course/com/example/selenium/spree/ShoppingSpreeTests.py
+++ python-course/com/example/selenium/spree/ShoppingSpreeTests.py
@@ -1,28 +1,45 @@
 import os
 import unittest
 
+from ddt import ddt, data, idata
 from selenium import webdriver
 
+driver_types = ["chrome", "firefox"]
 
+@ddt
 class ShoppingSpreeTests(unittest.TestCase):
-    def setUp(self):
-        # self.webDriver = webdriver.Firefox(executable_path="c:\\tools\\selenium\\geckodriver.exe")
-        # self.webDriver = webdriver.Chrome(executable_path="c:\\tools\\selenium\\chromedriver.exe")
-        # path = os.environ["webdriver_chrome_driver"]
-        # self.webDriver = webdriver.Chrome(executable_path=path)
-        # path = os.environ["webdriver_ie_driver"]
-        # self.webDriver = webdriver.Ie(executable_path=path)
-        self.webDriver = webdriver.Safari()
 
-    def testHomePage(self):
+    def create_driver(self, driver_type):
+        if "firefox" == driver_type:
+            path = os.environ["webdriver_firefox_driver"]
+            self.webDriver = webdriver.Firefox(executable_path=path)
+        elif "chrome" == driver_type:
+            path = os.environ["webdriver_chrome_driver"]
+            self.webDriver = webdriver.Chrome(executable_path=path)
+        elif "ie" == driver_type:
+            path = os.environ["webdriver_ie_driver"]
+            self.webDriver = webdriver.Ie(executable_path=path)
+        elif "edge" == driver_type:
+            path = os.environ["webdriver_edge_driver"]
+            self.webDriver = webdriver.Edge(executable_path=path)
+        elif "safari" == driver_type:
+            self.webDriver = webdriver.Safari()
+            
+    @idata(driver_types)
+    def testHomePage(self, driver_type):
+        self.create_driver(driver_type)
         self.webDriver.get("https://spreecommerce-demo.herokuapp.com")
         title = self.webDriver.title
         self.assertEquals("Spree Demo Site", title)
     
-    def testA(self):
+    @idata(driver_types)
+    def testA(self, driver_type):
+        self.create_driver(driver_type)
         self.assertTrue(True)
-    
-    def testB(self):
+
+    @idata(driver_types)
+    def testB(self, driver_type):
+        self.create_driver(driver_type)
         self.assertEquals(1, 1)
     
     def tearDown(self):
