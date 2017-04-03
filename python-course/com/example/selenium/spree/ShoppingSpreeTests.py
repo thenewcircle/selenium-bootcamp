af2b8850f09b84a9edeccd1007d8d6eb39e0a1c3
Jacob D. Parr <me@jacobparr.com>
2017-04-03 13:16:44 -0700

    Working with Chrome, Firefox and IE

--- python-course/com/example/selenium/spree/ShoppingSpreeTests.py
+++ python-course/com/example/selenium/spree/ShoppingSpreeTests.py
@@ -1,26 +1,30 @@
+import os
 import unittest
 
+from selenium import webdriver
 
 class ShoppingSpreeTests(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        print("Before class")
-
     def setUp(self):
-        print("Before method")
+        # path = os.environ["webdriver_firefox_driver"]
+        # self.webDriver = webdriver.Firefox(executable_path=path)
+        #
+        # path = os.environ["webdriver_chrome_driver"]
+        # self.webDriver = webdriver.Chrome(executable_path=path)
+        #
+        path = os.environ["webdriver_ie_driver"]
+        self.webDriver = webdriver.Ie(executable_path=path)
 
     def testA(self):
-        print("Test A")
-        self.assertEquals(1, 2)
+        self.assertTrue(True)
 
     def testB(self):
-        print("Test B")
         self.assertEquals(1, 1)
 
+    def testHomePage(self):
+        self.webDriver.get("https://selenium.jacobparr.com")
+        title = self.webDriver.title
+        self.assertEquals("Spree Demo Site", title)
+    
     def tearDown(self):
-        print("After method\n")
-
-    @classmethod
-    def tearDownClass(cls):
-        print("After class")
+        self.webDriver.quit()
